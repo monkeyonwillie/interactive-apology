eeb5db3e71db5e5f919a81ebe31671b316970f86
monkeyonwillie <williesocialclub@gmail.com>
2024-05-18 02:50:24 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -43,16 +43,10 @@ document.addEventListener('DOMContentLoaded', () => {
     function handleYes() {
         yesCount++;
 
-        if (yesCount === 3) {
-            characterImage.style.display = 'none'; // Remove the image after pressing yes three times
-        }
-
         if (yesCount < 5) {
             questionElement.innerText = yesMessages[yesCount - 1];
-        } else if (yesCount === 5) {
-            displaySweetMessages();
         } else {
-            displayHeartfeltMessage();
+            displaySweetMessages();
         }
     }
 
@@ -67,9 +61,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function displaySweetMessages() {
-        questionElement.innerText = yesMessages[yesMessages.length - 1];
-        buttonsDiv.innerHTML = ''; // Remove the buttons
-
         extraSweetMessages.forEach(message => {
             const p = document.createElement('p');
             p.innerText = message;
@@ -77,15 +68,9 @@ document.addEventListener('DOMContentLoaded', () => {
             container.appendChild(p);
         });
 
-        // Display the heartfelt message after all sweet messages
-        setTimeout(displayHeartfeltMessage, 5000);
-    }
-
-    function displayHeartfeltMessage() {
-        const heartfeltMessage = document.createElement('p');
-        heartfeltMessage.classList.add('heartfelt');
-        heartfeltMessage.innerText = "Thank you for giving us a chance to make things right. Your understanding and kindness mean the world to me.";
-        container.appendChild(heartfeltMessage);
+        // Swap Cupids
+        document.querySelector('.cupid.top-left').src = 'images/cupid3.png';
+        document.querySelector('.cupid.top-right').src = 'images/cupid4.png';
     }
 
     function explodeHearts() {
@@ -108,9 +93,9 @@ document.addEventListener('DOMContentLoaded', () => {
     function showScaryMessage() {
         document.body.innerHTML = `
             <div class="scary-background">
-                <img src="https://www.pngall.com/wp-content/uploads/15/Baki-PNG-Image-HD.png" alt="Scary Background">
                 <p class="scary">You are going to die</p>
             </div>
         `;
     }
 });
+        
